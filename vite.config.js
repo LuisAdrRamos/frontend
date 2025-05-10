3e4e2f7cd418c17a5fc6cf99fe16d5f9a219ce83
safronjos <guerralovatojosue@hotmail.com>
2025-05-10 11:15:20 -0500

    Cambios para produccion

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // Si tu sitio está en un subdirectorio, ajusta la ruta
   plugins: [react()],
   build: {
     rollupOptions: {
