9db2ecd26f4229e612d4442c4e064b1a434916e0
safronjos <guerralovatojosue@hotmail.com>
2025-05-10 11:09:09 -0500

    Despliegue produccion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor' // Separa las dependencias en un archivo aparte
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Ajusta el límite según lo necesario
+  }
 })
