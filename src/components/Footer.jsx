672fed1513337619f5d8ddf319490c303a991d7b
safronjos <guerralovatojosue@hotmail.com>
2025-05-17 15:35:44 -0500

    Cmbios Produccion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import "../styles/footer.css";
-import { FaFacebookF, FaInstagram, FaTwitter, FaGoogle, FaYoutube } from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaTwitter, FaGoogle, FaYoutube, FaTiktok, FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="social-icons">
-                <a href="https://www.facebook.com/"><FaFacebookF /></a>
-                <a href="#"><FaInstagram /></a>
-                <a href="#"><FaTwitter /></a>
-                <a href="#"><FaGoogle /></a>
-                <a href="#"><FaYoutube /></a>
+                <a href="https://www.instagram.com/megadisfraz_yaruqui?igsh=dHJwd3hmaHU2aTNq"><FaInstagram /></a>
+                <a href="https://x.com/Megadisfraz?t=IxW60maTBR2FoOO_RNDDeQ&s=09"><FaTwitter /></a>
+                <a href="https://www.tiktok.com/@megadisfraz_yaruqui?_t=ZM-8wGmbrvw46Y&_r=1"><FaTiktok /></a>
             </div>
             <ul className="footer-links">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">News</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact Us</a></li>
-                <li><a href="#">Our Team</a></li>
+                <li><a href="https://wa.me/593939232766"><FaWhatsapp/>Whatsapp</a></li>
+                <li><a href="webmegadisfraz513@gmail.com">Gmail</a></li>
             </ul>
             <p className="footer-text">© 2025 Megadisfraz</p>
         </footer>
